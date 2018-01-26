fffe1a5690d3f75aaf995489021a1c894791732d
Chase Southard <chase.southard@gmail.com>
2018-01-25 21:31:31 -0500

    using underscore

--- lib/nist_randomness_beacon/record.rb
+++ lib/nist_randomness_beacon/record.rb
@@ -1,10 +1,12 @@
 module NISTRandomnessBeacon
   class Record
+    require_relative 'underscore'
+    using Underscore
     attr_reader :version, :frequency, :time_stamp, :seed_value,
                 :previous_output_value, :signature_value, :output_value,
                 :status_code
 
-    def initialize args={}
+    def initialize(args={})
       args.each do |k, v|
         instance_variable_set("@#{k}".underscore, v) unless v.nil?
       end
